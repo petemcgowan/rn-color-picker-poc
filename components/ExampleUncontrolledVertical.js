83dadaf89dc1901704d7bc3c92ea8d2700f3ae54
Pete <peteburquette@gmail.com>
2022-03-21 00:59:28 +0000

    first commit

---
+++ components/ExampleUncontrolledVertical.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { ColorPicker } from "react-native-color-picker";
+
+export const ExampleUncontrolledVertical = () => (
+  <View style={{ flex: 1, padding: 45, backgroundColor: "#212021" }}>
+    <Text style={{ color: "white" }}>
+      React Native Color Picker - Uncontrolled
+    </Text>
+    <ColorPicker
+      oldColor="purple"
+      onColorSelected={(color) => alert(`Color selected: ${color}`)}
+      style={{ flex: 1 }}
+    />
+  </View>
+);
---
+++ readme.md
@@ -0,0 +1,7 @@
+This POC tests the react native color picker and if it does what I would like (pick a theme color):
+
+https://github.com/instea/react-native-color-picker
+
+However this looks deprecated, there are issues with Slider. A good example of a POC that proves that maybe it's a good idea not to use it.
+
+I'm gonna try and use react-native-wheel-color-picker instead. Because I DO want the slider, and the whole point of using an external package is it does its job!
